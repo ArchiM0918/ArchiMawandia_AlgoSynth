49081b71f3106a3fae8169e8e1dc3955fb7ab57b
Archi <mawandiaarchi@gmail.com>
2025-12-12 16:36:43 +0530

    Add files via upload

---
+++ Assignment 0 part 2.py
@@ -0,0 +1,10 @@
+numbers = []
+count = 0
+while count < 5:
+    num = int(input(f"Enter number {count + 1}: "))
+    numbers.append(num)
+    count = count + 1
+    if numbers[count-1] % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
